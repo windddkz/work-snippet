3898e4a9565f85a08eac67afa29e4de33c93d6a6
windddkz <36404594+windddkz@users.noreply.github.com>
2026-04-15 10:48:43 +0800

    Update entrypoint.sh

--- build-dev/entrypoint.sh
+++ build-dev/entrypoint.sh
@@ -4,8 +4,5 @@ set -e
 # 1. 自动生成 SSH 主机密钥 (避免容器启动时 sshd 崩溃)
 ssh-keygen -A 2>/dev/null
 
-# 2. 启动 SSH 守护进程 (在后台运行)
-/usr/sbin/sshd
-
-# 3. 将控制权交给 Dockerfile 中 CMD 指定的命令 (拉起 Zellij)
+# 2. 将控制权移交给 Dockerfile 中 CMD 指定的命令 (此时将拉起 sshd -D)
 exec "$@"
